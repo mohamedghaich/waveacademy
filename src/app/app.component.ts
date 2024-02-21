e00b1374c22a2b4c6b22b9e649d40df35c42c021
mohamedghaich <mohamed.10ghaich@gmail.com>
2024-02-21 21:31:13 +0100

    ssr remove 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'waveac';
+  loading: boolean = false;
+
+  fetchData() {
+    this.loading = true;
+    // Perform data fetching operation
+    // Once operation is complete, set loading to false
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
 }
